a7ff11f6036601d418784c3addaf79eee2cdb63e
hOOANGhUUUY <162947918+hOOANGhUUUY@users.noreply.github.com>
2025-08-27 14:16:29 +0700

    Refactor LoginSuccessModal to comment out SuccessModal rendering

--- src/components/common/LoginSuccessModal.tsx
+++ src/components/common/LoginSuccessModal.tsx
@@ -33,13 +33,14 @@ export const LoginSuccessModal: React.FC<LoginSuccessModalProps> = ({
   );
 
   return (
-    <SuccessModal
-      isOpen={isOpen}
-      onClose={onClose}
-      title="Đăng nhập thành công! haha"
-      message={userName ? `Chào mừng ${userName} đến với Moo Beef Steak!` : "Chào mừng bạn đến với Moo Beef Steak!"}
-      icon={loginIcon}
-    />
+    <div></div>
+    // <SuccessModal
+    //   isOpen={isOpen}
+    //   onClose={onClose}
+    //   title="Đăng nhập thành công! haha"
+    //   message={userName ? `Chào mừng ${userName} đến với Moo Beef Steak!` : "Chào mừng bạn đến với Moo Beef Steak!"}
+    //   icon={loginIcon}
+    // />
   );
 };
 
